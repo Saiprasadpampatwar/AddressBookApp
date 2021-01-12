77a4f279605a8e11571df6557955d3383f7d1aa3
SaiprasadPampatwar <pampatwarsaiprasad321@gmail.com>
2021-01-12 13:09:15 +0530

    [Saiprasad]Added: ability to view details from local storage  in home page

--- js/home.js
+++ js/home.js
@@ -1,35 +1,38 @@
-let addressbookList ;
+let contactList ;
 window.addEventListener('DOMContentLoaded',(event)=>{
-  //addressbookList = getAddressDataFromStorages();
-  createInnerHtml();
+   contactList = getAddressDataFromStorages();
+   document.querySelector('.emp-count').textContent = contactList.length;
+    createInnerHtml();
+  
 });
 
 const getAddressDataFromStorages = () =>{
-  return localStorage.getItem('AddressBookList')? JSON.parse(localStorage.getItem("AddressBookList")) : [];
+  return localStorage.getItem('ContactList')? JSON.parse(localStorage.getItem("ContactList")) : [];
   }
 
 const createInnerHtml= () => {
-  //if (addressbookList.length == 0) return;
-  const headerHtml = "<th>Full Name </th> <th>Address</th> <th>City</th> <th>State</th>"+
-                        "<th>Zip</th> <th>Phone Number</th> <th>Actions</th>";
-  let innerHtml = `${headerHtml}`;
+    
+    const headerHtml = "<th>Full Name </th> <th>Address</th> <th>City</th> <th>State</th>"+
+                            "<th>Zip</th> <th>Phone Number</th> <th>Actions</th>";
+    if (contactList.length == 0) return;
+    let innerHtml = `${headerHtml}`;
 
-  //for(const contact of addressbookList){
-    innerHtml = `${innerHtml}
-      <tr>
-        <td>Saiprasad Pampatwar</td>
-        <td>Venkatesh Nagar wadi</td>
-        <td>Nanded</div>
-        <td>Maharstra</td>
-        <td>431602</td>
-        <td>8888888888</td>
-        <td>
-          <img id="1" onclick="remove(this)" alt="delete" src="../assets/icons/delete-black-18dp.svg">
-          <img id="1"  onclick="update(this)" alt="edit" src="../assets/icons/create-black-18dp.svg">
-        </td>
-      </tr>
-    `;
-
+    for(const contact of contactList){
+        innerHtml = `${innerHtml}
+        <tr>
+            <td>${contact._name}</td>
+            <td>${contact._address}</td>
+            <td>${contact._city}</div>
+            <td>${contact._state}</td>
+            <td>${contact._zip}</td>
+            <td>${contact._phone}</td>
+            <td>
+            <img id="1" onclick="remove(this)" alt="delete" src="../assets/icons/delete-black-18dp.svg">
+            <img id="1"  onclick="update(this)" alt="edit" src="../assets/icons/create-black-18dp.svg">
+            </td>
+        </tr>
+        `;
+    }
     document.querySelector('#table-display').innerHTML = innerHtml;
-//}
+
 }
